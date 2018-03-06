c40d9c622c87909fe42130ec402caf1d2924f5ff
Tobias Paepke <tobias.paepke@paepke.net>
2018-03-06 11:03:13 +0100

    use correct pin layout for EggBot 2018 Edition

--- config.h
+++ config.h
@@ -1,20 +1,20 @@
 #pragma once
 
 // Rotational Stepper: ("X")
-#define X_STEP_PIN 7
-#define X_DIR_PIN 4
-#define X_ENABLE_PIN 8
+#define X_STEP_PIN 11
+#define X_DIR_PIN 10
+#define X_ENABLE_PIN 9
 
 // Pen Stepper:        ("Y")
-#define Y_STEP_PIN 6
-#define Y_DIR_PIN 3
-#define Y_ENABLE_PIN 8
+#define Y_STEP_PIN 8
+#define Y_DIR_PIN 7
+#define Y_ENABLE_PIN 6
 
 // Servo
-#define SERVO_PIN A5 // "SpnEn"
+#define SERVO_PIN A1 // "SpnEn"
 #define ENGRAVER_PIN A4 // "SpnDir"
 
 // Buttons
 #define PRG_BUTTON_PIN A2 // PRG button ("Abort")
-#define PEN_TOGGLE_BUTTON_PIN A1 // pen up/down button ("Hold")
+#define PEN_TOGGLE_BUTTON_PIN A3 // pen up/down button ("Hold")
 #define MOTORS_BUTTON_PIN A0 // motors enable button ("Resume")
