1777609ddb728f1a86d5158fc2a82c7be3dfe1c3
Justin Otherguy <justin@justinotherguy.org>
2018-03-20 23:20:40 +0100

    Update config.h (buttons, move y -> z socket)
    enable buttons, move Y axis to Z socket

--- config.h
+++ config.h
@@ -6,15 +6,15 @@
 #define X_ENABLE_PIN 8 // only one ENABLE pin for all axes
 
 // Pen Stepper:        ("Y")
-#define Y_STEP_PIN 3
-#define Y_DIR_PIN 6
-#define Y_ENABLE_PIN 12 // this is intentionally set to some non connected pin as the CNC board has the ENABLE pins for all axes connected anyway; enabling one is sufficient, so
+#define Y_STEP_PIN 4
+#define Y_DIR_PIN 7
+#define Y_ENABLE_PIN 11 // this is intentionally set to some non connected pin as the CNC board has the ENABLE pins for all axes connected anyway; enabling one is sufficient, so
 
 // Servo
-#define SERVO_PIN A1 // "SpnEn"
+#define SERVO_PIN 12 // "SpnEn"
 #define ENGRAVER_PIN A4 // "SpnDir"
 
 // Buttons
-#define PRG_BUTTON_PIN A2 // PRG button ("Abort")
-#define PEN_TOGGLE_BUTTON_PIN A3 // pen up/down button ("Hold")
-#define MOTORS_BUTTON_PIN A0 // motors enable button ("Resume")
+#define PRG_BUTTON_PIN A0 // PRG button ("Abort")
+#define PEN_TOGGLE_BUTTON_PIN A1 // pen up/down button ("Hold")
+#define MOTORS_BUTTON_PIN A2   // motors enable button ("Resume")
